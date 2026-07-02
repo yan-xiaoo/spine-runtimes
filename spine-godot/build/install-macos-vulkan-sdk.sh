0acb7d72f6f5b538cec8e7a1e58ea48e45613b18
Yanxiao Li <liyanxiao050804@126.com>
2026-07-02 16:41:53 +0800

    Update Vulkan SDK download link to version 1.3.290
    Fix build script linking to a SDK file that doesn't exist.

--- spine-godot/build/install-macos-vulkan-sdk.sh
+++ spine-godot/build/install-macos-vulkan-sdk.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Download and install the Vulkan SDK.
-curl -L "https://sdk.lunarg.com/sdk/download/1.3.275.0/mac/vulkansdk-macos-1.3.275.0.dmg" -o /tmp/vulkan-sdk.dmg
+curl -fL "https://sdk.lunarg.com/sdk/download/1.3.290.0/mac/vulkansdk-macos-1.3.290.0.dmg" -o /tmp/vulkan-sdk.dmg
 hdiutil attach /tmp/vulkan-sdk.dmg -mountpoint /Volumes/vulkan-sdk
 /Volumes/vulkan-sdk/InstallVulkan.app/Contents/MacOS/InstallVulkan \
     --accept-licenses --default-answer --confirm-command install com.lunarg.vulkan.ios
